e0c1e2997367283d74c9c3a98125d9cd520f90af
Fayaz <kfayaz1407@gmail.com>
2024-03-06 13:08:02 +0530

    Completed Avatar selection UI

--- src/Screens/ChooseAvatar.js
+++ src/Screens/ChooseAvatar.js
@@ -1,22 +1,91 @@
 
+let avatars = [
+    {
+        img:'superhero.png',
+        alt:'I am Ironman!'
+    },
+    {
+        img: 'bear.png',
+        alt:'Bear',
+        title:'Cuddle Bear'
+    },
+    {
+        img:'cat.png',
+        alt:'Cat',
+        title:"Snow cat"
+    },
+    {
+        img: 'chewing-gum.png',
+        alt:'Chewing gum',
+        title:'Cool Dude'
+    },
+    {
+        img: 'gamer.png',
+        alt:'Gamer',
+        title:"Gamer Boy"
+    },
+    {
+        img:'man (1).png',
+        alt: 'Man Image 1',
+        title:"I am Nerd"
+    },
+    {
+        img:'man.png',
+        alt:'Man Image',
+    },
+    {
+        img:'penguin.png',
+        alt:'Penguin Image'
+    },
+    {
+        img:'rabbit.png',
+        alt:'Rabbit',
+    },
+    {
+        img:'spiderman.png',
+        alt:'Spiderman'
+    },
+    {
+        img: 'batman.png',
+        alt:'batman',
+        title:"I'm Vengence"
+    },
+    {
+        img:'woman (1).png',
+        alt:'Woman 1',
+    },
+    {
+        img:'woman (2).png',
+        alt:'Woman 2'
+    },
+    {
+        img:'woman (3).png',
+        alt:'woman 3'
+    },
+    {
+        img:'woman (4).png',
+        alt:'Woman 4'
+    },
+    {
+        img:'woman.png',
+        alt:'woman'
+    }
+]
 
 const ChooseAvatar = ()=>{
     return(
         <>
             <div className="w-full h-screen grid place-items-center">
-                <div className="w-3/4 bg-white h-5/6 rounded border shadow-2xl flex flex-wrap p-3 overflow-y-scroll">
-                    <div className="bg-red-300 w-[150px] h-[150px] rounded-full">
-                        <img src={require("../Assets/batman.png")} className="rounded-full"/>
-                    </div>
-                    <div className="bg-red-300 w-[150px] h-[150px] rounded-full">
-                        <img src={require("../Assets/bear.png")} className="rounded-full"/>
-                    </div>
-                    <div className="bg-red-300 w-[150px] h-[150px] rounded-full">
-                        <img src={require("../Assets/cat.png")} className="rounded-full"/>
-                    </div>
-                    <div className="bg-red-300 w-[150px] h-[150px] rounded-full">
-                        <img src={require("../Assets/chewing-gum.png")} className="rounded-full"/>
-                    </div>
+                <div className="w-3/4 bg-white h-5/6 rounded border shadow-2xl flex flex-wrap p-5 overflow-y-scroll">
+                    {
+                        avatars.map((item,index)=>{
+                            return(
+                                <div key={index} className="w-[110px] cursor-pointer h-[110px] flex flex-row justify-center items-center rounded-full mx-10 my-5">
+                                    <img style={{width:'100px',height:'100px',objectFit:'contain'}}  src={require(`../Assets/${item.img}`)} alt={item.alt} className="rounded-full bg-white"/>
+                                </div>
+                            )
+                        })
+                    }
                 </div>
             </div>
         </>
